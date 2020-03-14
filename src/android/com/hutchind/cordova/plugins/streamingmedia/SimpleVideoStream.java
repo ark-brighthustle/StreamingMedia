0cdc453db2508dfcd1188a77f5f79082a1e43f61
evilrat <50332657+evilrat@users.noreply.github.com>
2020-03-15 01:13:55 +0530

    Update SimpleVideoStream.java

--- src/android/com/hutchind/cordova/plugins/streamingmedia/SimpleVideoStream.java
+++ src/android/com/hutchind/cordova/plugins/streamingmedia/SimpleVideoStream.java
@@ -119,7 +119,6 @@ MediaPlayer.OnErrorListener, MediaPlayer.OnBufferingUpdateListener {
 		mMediaPlayer = mp;
 		mMediaPlayer.setOnBufferingUpdateListener(this);
 		mVideoView.requestFocus();
-		mVideoView.seekTo(1000); 
         	mMediaPlayer.seekTo(1000);
 		mVideoView.start();
 		mVideoView.postDelayed(checkIfPlaying, 0);
